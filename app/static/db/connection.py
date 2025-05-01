96ac0d13dc96a0638b13d9c5a1d5562d3a76f810
Eduardo Becerra <josebecerra.0870@gmail.com>
2025-05-01 16:10:22 -0700

    Replace for a new DB

--- app/static/db/connection.py
+++ app/static/db/connection.py
@@ -6,9 +6,9 @@ def get_db_connection():
     try:
         conn = psycopg2.connect(
             dbname="minimal_products",
-            user="root",
-            password="iPWhoofYUeOfw1Ss9rC1P5bFBSQdNRR3",
-            host="dpg-cvhit49c1ekc738c4a00-a.oregon-postgres.render.com",
+            user="minimalproducts_user",
+            password="16HLKUZiQVLO7Go2tlZ15OjdhZpJ5WlR",
+            host="dpg-d09vb5qdbo4c73e5chvg-a.oregon-postgres.render.com",
             port="5432"
         )
         cur = conn.cursor() #Crea un cursor para ejecutar consultas
